d6b44d847330499a2f3811d7bf56db777123b20e
DeSpiLe <despile05@gmail.com>
2024-11-17 15:29:52 +0500

    PostgreSQL

--- app/config.py
+++ app/config.py
@@ -1,8 +1,14 @@
 import os
 
 class Config:
+    # Общие настройки
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///bicycle_store.db')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
+    # Строка подключения к базе данных (подставьте External Database URL)
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql://bike_store_64y3_user:heCSFrdNYHiEYD7G8QPpuwNfeE2at9hg@dpg-csss8mggph6c7398j6fg-a.oregon-postgres.render.com/bike_store_64y3'
+    )
+
+    # Секретный ключ для безопасности
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
